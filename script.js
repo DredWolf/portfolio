4d13d75860e02d4ea542dc0a455b4c3aa5444d89
Ruslan <60795195+DredWolf@users.noreply.github.com>
2022-08-23 16:28:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function changeToRUS() {
+  let engElements = document.querySelectorAll(".eng");
+  let ruElements = document.querySelectorAll(".ru");
+  let ruBtn = document.querySelector(".language-picker__ru");
+  let engBtn = document.querySelector(".language-picker__eng");
+
+  for (let i = 0; i < engElements.length; i++) {
+    engElements[i].style.display = "none";
+  }
+  for (let i = 0; i < ruElements.length; i++) {
+    ruElements[i].style.display = "block";
+    ruBtn.style.color = 'black';
+    engBtn.style.color = '#828282' 
+  }
+}
+
+function changeToENG() {
+  let engElements = document.querySelectorAll(".eng");
+  let ruElements = document.querySelectorAll(".ru");
+  let ruBtn = document.querySelector(".language-picker__ru");
+  let engBtn = document.querySelector(".language-picker__eng");
+  for (let i = 0; i < ruElements.length; i++) {
+    ruElements[i].style.display = "none";
+  }
+  for (let i = 0; i < engElements.length; i++) {
+    engElements[i].style.display = "block";
+    ruBtn.style.color = '#828282';
+    engBtn.style.color = 'black' 
+  }
+}
